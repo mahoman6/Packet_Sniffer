1481e053392e9c5d8dd88e1dc319e719b508688b
Mahdi_A <12f121t7@gmail.com>
2022-02-01 01:17:06 +0200

    Add MySQL file that Initializes the Database

---
+++ initDB.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE pcap;
+use pcap;
+CREATE TABLE sessions (
+	name VARCHAR(20) NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    device VARCHAR(20) NOT NULL,
+    filter VARCHAR(30) NOT NULL,
+    time TIMESTAMP NOT NULL,
+    dur INT NOT NULL
+);
+CREATE TABLE packets (
+	s_addr VARCHAR(25) NOT NULL,
+    d_addr VARCHAR(25) NOT NULL,
+    info VARCHAR(1000) NOT NULL,
+    session_id INT NOT NULL,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+);
